9fd12c0203ed33cc29ed4d26911547260c457704
Amrish Matura <amrishmatura95@gmail.com>
2026-02-21 18:53:49 +0530

    Update

--- src/Basics/string.java
+++ src/Basics/string.java
@@ -28,8 +28,33 @@ public class string {
 
         String s2 = new String("Amrish").intern();
         System.err.println(s2==s);  // false of same reason of new string
-        
+
         System.err.println(s2==x);  // true due to intern() , take pool address now 
+
+        // Compare value
+
+        System.err.println("Comparing value - " + s2.equals(s));
+    
+        
+        // String are immutable  it crates new object or memeory every time when change are made
+        String str1="Aman";
+        System.err.println(str1);
+        str1="Aman is a goood boy";
+
+        //stringbuffer - methods are synchronization provide lock so othere thread cannot accces it if one is already using it
+
+        System.err.println(str1);
+        StringBuffer str = new StringBuffer("Amrish mAtura");
+        str.append(" a btech student");
+        System.err.println(str);
+
+
+        /*stringbuilder _ it is a modern class , its method are not synchronized 
+        so it will not lock its method for threads */
+
+        StringBuilder str3 = new StringBuilder("Amrish mAtura");
+        str3.append(" a btech student");
+        System.err.println(str);
 
     }
 }
